944509c297834402580051c1858342c89237b7c6
ValeRuiz33 <lizethv_rivera@soy.sena.edu.co>
2024-04-06 17:11:26 -0500

    Add(conf): add controllers, models and conexion with the data base
    add controller registration, models registration and response and conexion with the data base

---
+++ Hoja_Vida/Hoja_Vida/Controllers/RegistrationController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Hoja_Vida.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Hoja_Vida.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public RegistrationController(IConfiguration configuration)
+        {
+            _configuration= configuration;
+
+        }
+
+        
+        [HttpPost]
+        [Route("registration")]
+        public string registration(Registration registration)
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("HojaVida").ToString());
+            SqlCommand cmd = new ("INSERT INTO Registration(UserName, Password, Email, IsActive) VALUES('" + registration.UserName + "', '" + registration.Password + "', '" + registration.Email + "', '" + registration.IsActive + "')", con);
+            con.Open();
+            int i= cmd.ExecuteNonQuery();
+            con.Close();
+            if (i > 0) 
+            {
+                return "Data inserted";
+            }
+            else
+            {
+                return "Error";
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public string login(Registration registration)
+        {
+            SqlConnection co = new SqlConnection(_configuration.GetConnectionString("HojaVida").ToString());
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Registration WHERE UserName= '" + registration.UserName + "' AND Password = '" + registration.Password + "' AND IsActive= 1 ", co);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if(dt.Rows.Count > 0)
+            {
+                return "Valid User";
+            }
+            else
+            {
+                return "Invalid User";
+            }
+        }
+    }
+}
---
+++ Hoja_Vida/Hoja_Vida/Models/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace Hoja_Vida.Models
+{
+    public class Response
+    {
+        public int statusCode { get; set; }
+        public string statusMessage { get; set; }
+    }
+}
